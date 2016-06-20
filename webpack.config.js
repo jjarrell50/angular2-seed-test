2880feb7de66fa9cfe918da99eb0b0ab599eae56
jjarrell50 <jeff.jarrell@gmail.com>
2016-06-20 18:49:56 -0400

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ var webpackConfig = {
     'polyfills': './src/polyfills.ts',
     'vendor':    './src/vendor.ts',
     'app':       './src/app.ts',
+    'polymer':    './dist/polymerimports.html'
   },
 
   output: {
